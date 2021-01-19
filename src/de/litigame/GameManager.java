d2fa273311d27380e61ad757642cc0cb8679397f
T0m2223 <64265856+T0m2223@users.noreply.github.com>
2021-01-19 17:44:50 +0100

    solved conflict

--- src/de/litigame/GameManager.java
+++ src/de/litigame/GameManager.java
@@ -8,7 +8,6 @@ import de.gurkenlabs.litiengine.environment.Environment;
 import de.litigame.entities.Player;
 import de.litigame.graphics.PlayerCamera;
 import de.litigame.input.InputManager;
-import de.litigame.items.Items;
 
 public class GameManager {
 
@@ -27,14 +26,6 @@ public class GameManager {
 
 		switchToMap("map1");
 		Game.world().environment().getSpawnpoint("spawn").spawn(Player.getInstance());
-
-<<<<<<< Updated upstream
-		Player.getInstance().hotbar.addItem(Items.getItem("sword"));
-		Player.getInstance().hotbar.addItem(Items.getItem("shield"));
-=======
-		Player.getInstance().hotbar.addItem(Items.getItem("potion"));
-		Player.getInstance().hotbar.addItem(Items.getItem("sword"));
->>>>>>> Stashed changes
 
 		switchToState(GameState.INGAME);
 	}
