a95d61b6d1ee2930227e8881a76ff5450efef9aa
alcoholicwaluigi <simoneesposito1gb@gmail.com>
2021-01-28 15:54:03 +0100

    Minor corrections and simplifications

--- src/de/litigame/gui/MainMenu.java
+++ src/de/litigame/gui/MainMenu.java
@@ -1,19 +1,11 @@
 package de.litigame.gui;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 
-import com.sun.tools.javac.Main;
 import de.gurkenlabs.litiengine.Game;
-import de.gurkenlabs.litiengine.graphics.ImageRenderer;
 import de.gurkenlabs.litiengine.gui.ImageComponent;
 import de.gurkenlabs.litiengine.gui.Menu;
 import de.gurkenlabs.litiengine.graphics.Spritesheet;
-import de.gurkenlabs.litiengine.gui.screens.GameScreen;
 import de.gurkenlabs.litiengine.gui.screens.Screen;
 import de.litigame.Images;
-
-import javax.swing.*;
 
 public class MainMenu extends Screen {
 
@@ -31,16 +23,4 @@ public class MainMenu extends Screen {
         getComponents().add(bkgr);
         getComponents().add(menu);
     }
-
-    //@Override
-
-    //public void render(Graphics2D g) {
-        //Spritesheet img = new Spritesheet(Images.get("menu_item"), "screens/menu_item.jpg", 1, 1);
-        /*System.out.println(this.menu.getBackground())
-          ImageRenderer.render(g, this.menu.getBackground().getImage().getScaledInstance(1920,1080,0),0,0);
-          menu.prepare();
-          ImageRenderer.render(g, Images.get("menu").getScaledInstance(1920,1080,0),0,0);
-          Player.getInstance().hotbar.render(g);
-        menu.render(g);*/
-    //}
 }
