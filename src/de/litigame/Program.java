94a2dcc75121785a6d28079224fb1781e51f98c0
alcoholicwaluigi <simoneesposito1gb@gmail.com>
2021-01-29 14:12:59 +0100

    Added Main menu and Settings menu

--- src/de/litigame/Program.java
+++ src/de/litigame/Program.java
@@ -8,6 +8,7 @@ import de.gurkenlabs.litiengine.configuration.DisplayMode;
 import de.gurkenlabs.litiengine.resources.Resources;
 import de.litigame.gui.IngameScreen;
 import de.litigame.gui.MainMenu;
+import de.litigame.gui.SettigsScreen;
 import de.litigame.items.Items;
 
 public class Program {
@@ -19,7 +20,8 @@ public class Program {
 		Items.init(new File("items.json"));
 		Game.config().graphics().setDisplayMode(DisplayMode.BORDERLESS);
 		Game.screens().add(new IngameScreen());
-		Game.screens().add(new MainMenu("menu_item"));
+		Game.screens().add(new MainMenu("menu_item", "Start Game", "Load Game", "Settings", "Exit Game"));
+		Game.screens().add(new SettigsScreen("menu_item","Done"));
 		Game.screens().display("menu");
 		GameManager.init();
 		Game.start();
--- src/de/litigame/gui/SettigsScreen.java
+++ src/de/litigame/gui/SettigsScreen.java
@@ -1,30 +1,26 @@
 package de.litigame.gui;
 
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.graphics.Spritesheet;
 import de.gurkenlabs.litiengine.gui.ImageComponent;
 import de.gurkenlabs.litiengine.gui.Menu;
-import de.gurkenlabs.litiengine.graphics.Spritesheet;
 import de.gurkenlabs.litiengine.gui.screens.Screen;
 import de.litigame.Images;
 
-public class MainMenu extends Screen {
-
+public class SettigsScreen extends Screen {
     private Menu menu;
-
-    public MainMenu(final String image, String... items) {
+    public SettigsScreen(final String image, String... items) {
         this((double) Game.window().getWidth()/2-415,(double)Game.window().getHeight()/(2* items.length),830,199* items.length, image, items);
     }
 
-    public MainMenu(final double x, final double y, final double width, final double height, String image, final String... items){
-        super("menu");
+    public SettigsScreen(final double x, final double y, final double width, final double height, String image, final String... items){
+        super("settings");
         Spritesheet item = new Spritesheet(Images.get(image), "screens/"+image+".jpg", 830, 199);
         menu = new Menu(x,y,width,height, item, items);
         ImageComponent bkgr = new ImageComponent(0,0, Images.get("menu"));
         menu.prepare();
         menu.onChange(index -> {
-            if (index == 0) Game.screens().display("ingame");
-            else if (index == 2) Game.screens().display("settings");
-            else if (index == 3) System.exit(0);
+            if (index == 0) Game.screens().display("menu");
         });
         getComponents().add(bkgr);
         getComponents().add(menu);
