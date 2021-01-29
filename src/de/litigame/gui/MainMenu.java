dd5694a5602c0e9e6eae610293415b83c2617c8f
alcoholicwaluigi <simoneesposito1gb@gmail.com>
2021-01-29 15:58:47 +0100

    Generalised Sizes; Changed targetCompatibility

--- src/de/litigame/gui/MainMenu.java
+++ src/de/litigame/gui/MainMenu.java
@@ -12,7 +12,12 @@ public class MainMenu extends Screen {
     private Menu menu;
 
     public MainMenu(final String image, String... items) {
-        this((double) Game.window().getWidth()/2-415,(double)Game.window().getHeight()/(2* items.length),830,199* items.length, image, items);
+        this(   (double) (Game.window().getWidth()-Images.get(image).getWidth())/2,
+                (double)Game.window().getHeight()/(2* items.length),
+                Images.get(image).getWidth(),
+                Images.get(image).getHeight()*items.length,
+                image,
+                items);
     }
 
     public MainMenu(final double x, final double y, final double width, final double height, String image, final String... items){
