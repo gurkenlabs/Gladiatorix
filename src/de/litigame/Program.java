f944c9a8ff645a9654c070001f71871bdb3fc1e7
alcoholicwaluigi <simoneesposito1gb@gmail.com>
2021-01-29 18:47:27 +0100

    Eliminated Menu Parameters

--- src/de/litigame/Program.java
+++ src/de/litigame/Program.java
@@ -20,7 +20,7 @@ public class Program {
 		Items.init(new File("items.json"));
 		Game.config().graphics().setDisplayMode(DisplayMode.BORDERLESS);
 		Game.screens().add(new IngameScreen());
-		Game.screens().add(new MainMenu("menu_item", "Start Game", "Load Game", "Settings", "Exit Game"));
+		Game.screens().add(new MainMenu());
 		Game.screens().add(new SettigsScreen("menu_item","Done"));
 		Game.screens().display("menu");
 		GameManager.init();
--- src/de/litigame/gui/MainMenu.java
+++ src/de/litigame/gui/MainMenu.java
@@ -7,11 +7,13 @@ import de.gurkenlabs.litiengine.graphics.Spritesheet;
 import de.gurkenlabs.litiengine.gui.screens.Screen;
 import de.litigame.Images;
 
+import java.awt.image.BufferedImage;
+
 public class MainMenu extends Screen {
 
     private Menu menu;
-
-    public MainMenu(final String image, String... items) {
+    private final int cells = 4;
+    /*public MainMenu(final String image, String... items) {
         this(   (double) (Game.window().getWidth()-Images.get(image).getWidth())/2,
                 (double)Game.window().getHeight()/(2* items.length),
                 Images.get(image).getWidth(),
@@ -24,6 +26,27 @@ public class MainMenu extends Screen {
         super("menu");
         Spritesheet item = new Spritesheet(Images.get(image), "screens/"+image+".jpg", 830, 199);
         menu = new Menu(x,y,width,height, item, items);
+        ImageComponent bkgr = new ImageComponent(0,0, Images.get("menu"));
+        menu.prepare();
+        menu.onChange(index -> {
+            if (index == 0) Game.screens().display("ingame");
+            else if (index == 2) Game.screens().display("settings");
+            else if (index == 3) System.exit(0);
+        });
+        getComponents().add(bkgr);
+        getComponents().add(menu);
+    }*/
+    public MainMenu(){
+        super("menu");
+        BufferedImage img = Images.get("menu_item");
+        Spritesheet item = new Spritesheet(img, "screens/menu_item.jpg", img.getWidth(), img.getHeight());
+        menu = new Menu(
+                (double) (Game.window().getWidth()-img.getWidth())/2,
+                (double) Game.window().getHeight()/(2* cells),
+                img.getWidth(),
+                img.getHeight()*cells,
+                item,
+                "Start Game", "Load Game", "Settings", "Exit Game");
         ImageComponent bkgr = new ImageComponent(0,0, Images.get("menu"));
         menu.prepare();
         menu.onChange(index -> {
