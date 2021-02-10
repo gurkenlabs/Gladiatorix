7564ee3bd9d1f7c38a6381a42f1092c0442eab6a
marlonwood <marlonwood@outlook.de>
2021-02-10 21:49:46 +0100

    Added Map

---
+++ maps/resources/tilesets/arena1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="arena1" tilewidth="16" tileheight="16" tilecount="104" columns="13">
+ <image source="../Layer 1/arena1.png" trans="222222" width="208" height="128"/>
+</tileset>
---
+++ maps/resources/tilesets/tile-grass.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tile-grass" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <image source="../Layer 0/tile-grass.PNG" trans="222222" width="64" height="64"/>
+ <tile id="0" probability="0.2"/>
+ <tile id="1" probability="0.05"/>
+ <tile id="2" probability="0.2"/>
+ <tile id="3" probability="0.05"/>
+ <tile id="4" probability="0.05"/>
+ <tile id="5" probability="0.05"/>
+ <tile id="6" probability="0.05"/>
+ <tile id="7" probability="0.1"/>
+ <tile id="8" probability="0.2"/>
+ <tile id="9" probability="0.05"/>
+ <tile id="10" probability="0.05"/>
+ <tile id="11" probability="0.1"/>
+ <tile id="12" probability="0.2"/>
+ <tile id="13" probability="0.05"/>
+ <tile id="14" probability="0.1"/>
+ <tile id="15" probability="0.05"/>
+</tileset>
