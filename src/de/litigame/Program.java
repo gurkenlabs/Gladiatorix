0006d1bcb558afa28597333f93ea8db99420d8c7
marlonwood <marlonwood@outlook.de>
2021-03-04 14:50:31 +0100

    sounds + initialization

--- src/de/litigame/Program.java
+++ src/de/litigame/Program.java
@@ -1,19 +1,25 @@
 package de.litigame;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.resources.Resources;
 import de.litigame.gui.IngameScreen;
 import de.litigame.items.Items;
+import de.litigame.sounds.Sounds;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedAudioFileException, IOException  {
 		Game.init(args);
 		Resources.load("game.litidata");
 		Images.init(new File("images.txt"));
 		Items.init(new File("items.json"));
+		Sounds.init();
 		Game.screens().add(new IngameScreen());
 		GameManager.init();
 		Game.start();
