53a890161cfbf9032dbda36ce8eda158fde90398
T0m2223 <64265856+T0m2223@users.noreply.github.com>
2021-03-05 21:43:03 +0100

    added speed

--- src/de/litigame/items/Potion.java
+++ src/de/litigame/items/Potion.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.attributes.AttributeModifier;
 import de.gurkenlabs.litiengine.attributes.Modification;
+import de.litigame.entities.Player;
 import de.litigame.items.Item.Consumable;
 import de.litigame.items.Item.Stackable;
 
@@ -15,11 +17,19 @@ public class Potion extends Item implements Stackable, Consumable {
 
 	List<ConsumeListener> consumeListeners = new ArrayList<>();
 
+	private AttributeModifier mod;
+
 	public Potion(Map<String, String> itemInfo) {
 		super(itemInfo);
 		switch (itemInfo.get("potion_effect")) {
 		case "heal":
-			addConsumeListener(p -> p.getHitPoints().addModifier(new AttributeModifier<Integer>(Modification.ADD, 50)));
+			mod = new AttributeModifier<Integer>(Modification.ADD, 50);
+			addConsumeListener(p -> p.getHitPoints().addModifier(mod));
+			break;
+		case "speed":
+			mod = new AttributeModifier<Float>(Modification.MULTIPLY, 2);
+			addConsumeListener(p -> p.getVelocity().addModifier(mod));
+			Game.loop().perform(5000, () -> Player.getInstance().getVelocity().removeModifier(mod));
 			break;
 		}
 	}
